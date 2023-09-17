0faeeab98f2a27e4e38a305b2f156f0783516870
DanChick109 <145198452+DanChick109@users.noreply.github.com>
2023-09-17 14:39:03 +0300

    2

--- first/main.py
+++ first/main.py
@@ -1,1 +1,19 @@
+def main():
+    sides = [2, 56, 2, 768, 1, 3, 67, 2, 10, 187, 100, 4]
+    sides = sorted(sides, reverse=True)
 
+    S_max = 0
+    for i in range(len(sides)):
+        for j in range(i + 1, len(sides)):
+            for k in range(j + 1, len(sides)):  # Оптимизированный перебор
+                a, b, c = sides[i], sides[j], sides[k]
+                if (a + b > c) and (a + c > b) and (b + c > a):  # Проверка существования треугольника
+                    p = (a + b + c) * .5  # Находим полупериметр
+                    s = (p * (p - a) * (p - b) * (p - c)) ** .5  # Используем формулу Герона
+                    if s > S_max:
+                        S_max = s
+    print("Максимальная площадь треугольника - {}".format(S_max))
+
+
+if __name__ == "__main__":
+    main()
